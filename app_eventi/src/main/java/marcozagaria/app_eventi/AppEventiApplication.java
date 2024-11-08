f530e658b1f2b0a1fa15f9d2daca0cba41c6c992
Marco Zagaria <marcozagaria@live.com>
2024-11-08 10:54:27 +0100

    inizio

---
+++ app_eventi/src/main/java/marcozagaria/app_eventi/AppEventiApplication.java
@@ -0,0 +1,13 @@
+package marcozagaria.app_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventiApplication.class, args);
+	}
+
+}
---
+++ app_eventi/src/test/java/marcozagaria/app_eventi/AppEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.app_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
